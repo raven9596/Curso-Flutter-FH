fcf36aa551ed6f2237da2358a3da1383c06b5f22
Gilberto Acosta <acostagilberto95@gmail.com>
2021-06-01 19:26:02 -0400

    update input

---
+++ componentes/lib/src/pages/input_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  InputPage({Key key}) : super(key: key);
+
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs'),
+      ),
+       
+    );
+  }
+}
--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/input_page.dart';
 
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -15,6 +16,7 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'avatar': (BuildContext context) => AvatarPage(),
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+    'inputs': (BuildContext context) => InputPage(),
   };
 }
 
