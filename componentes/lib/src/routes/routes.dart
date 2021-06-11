f6c2be5e54417faaef7ee5cffa7325dcafce1c9c
Gilberto Acosta <acostagilberto95@gmail.com>
2021-06-11 11:36:52 -0400

    Update Components App

--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -6,6 +6,8 @@ import 'package:componentes/src/pages/avatar_page.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/slider_page.dart';
+import 'package:componentes/src/pages/listview_page.dart';
 
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -17,6 +19,8 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
+    'slider': (BuildContext context) => SliderPage(),
+    'list': (BuildContext context) => ListViewPage(),
   };
 }
 
